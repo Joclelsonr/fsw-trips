9343b2fe94e9e4c5c05a79532982809e3bef0519
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2023-07-05 23:49:37 -0300

    feat: :sparkles: add trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import React from "react";
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </>
   );
 }
