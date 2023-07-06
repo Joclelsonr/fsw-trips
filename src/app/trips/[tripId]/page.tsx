82893f6f3009fd2e694743ed5af8c15ed73df791
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2023-07-06 14:33:07 -0300

    feat: :sparkles: add trip description component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { prisma } from "@/lib/prisma";
+
 import TripDetailHeader from "./components/TripDetailHeader";
 import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -21,6 +23,7 @@ async function TripDetails({ params }: { params: { tripId: string } }) {
     <div className="container mx-auto">
       <TripDetailHeader trip={trip} />
       <TripReservation trip={trip} />
+      <TripDescription description={trip.description!} />
     </div>
   );
 }
